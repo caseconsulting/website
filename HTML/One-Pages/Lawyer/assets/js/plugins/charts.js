777151877469ab8007a3613783f85c8afaaf4eee
Fahad <fahada95@vt.edu>
2017-06-07 14:22:13 -0400

    switch-sections: chart tweaks

--- HTML/One-Pages/Lawyer/assets/js/plugins/charts.js
+++ HTML/One-Pages/Lawyer/assets/js/plugins/charts.js
@@ -3,26 +3,19 @@ window.onload = function(){
 Chart.defaults.global.defaultFontColor = 'black';
 
 var ctx1 = document.getElementById("pie").getContext('2d');
-
+var colors = ["#2ecc71","#3498db","#95a5a6","#9b59b6","#f1c40f","#e74c3c","#34495e"];
+data = [12, 19, 3, 17, 28, 24, 7];
 var myChart = new Chart(ctx1, {
   type: 'doughnut',
   data: {
     labels: ["People who like it", "People who don't like it", "People who don't know what's going on", "People who don't care", "The Uninitiated", "Lord of the Ring fans", "Fell asleep during the movie"],
     datasets: [{
-      backgroundColor: [
-        "#2ecc71",
-        "#3498db",
-        "#95a5a6",
-        "#9b59b6",
-        "#f1c40f",
-        "#e74c3c",
-        "#34495e"
-      ],
+      backgroundColor: colors ,
       data: [12, 19, 3, 17, 28, 24, 7]
     }]
 
   }
-
+  
 
 });
 
