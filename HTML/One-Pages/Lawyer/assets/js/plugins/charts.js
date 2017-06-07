81db2797c096e425dfbff1591113b6a3d032abf1
Fahad <fahada95@vt.edu>
2017-06-07 13:44:47 -0400

    moved sections around; added second chart

--- HTML/One-Pages/Lawyer/assets/js/plugins/charts.js
+++ HTML/One-Pages/Lawyer/assets/js/plugins/charts.js
@@ -1,6 +1,10 @@
-var ctx = document.getElementById("chart").getContext('2d');
-Chart.defaults.global.defaultFontColor = 'white';
-var myChart = new Chart(ctx, {
+window.onload = function(){
+
+Chart.defaults.global.defaultFontColor = 'black';
+
+var ctx1 = document.getElementById("pie").getContext('2d');
+
+var myChart = new Chart(ctx1, {
   type: 'doughnut',
   data: {
     labels: ["People who like it", "People who don't like it", "People who don't know what's going on", "People who don't care", "The Uninitiated", "Lord of the Ring fans", "Fell asleep during the movie"],
@@ -16,8 +20,32 @@ var myChart = new Chart(ctx, {
       ],
       data: [12, 19, 3, 17, 28, 24, 7]
     }]
-    
+
   }
-  
-  
+
+
 });
+
+var ctx2 = document.getElementById("bar").getContext('2d');
+var myChart = new Chart(ctx2, {
+  type: 'bar',
+  data: {
+    labels: ["People who like it", "People who don't like it", "People who don't know what's going on", "People who don't care", "The Uninitiated", "Lord of the Ring fans", "Fell asleep during the movie"],
+    datasets: [{
+      backgroundColor: [
+        "#2ecc71",
+        "#3498db",
+        "#95a5a6",
+        "#9b59b6",
+        "#f1c40f",
+        "#e74c3c",
+        "#34495e"
+      ],
+      data: [12, 19, 3, 17, 28, 24, 7]
+    }]
+
+  }
+
+
+});
+};
