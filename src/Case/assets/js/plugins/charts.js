c853fb5730e8b0ba229960a7b0b2432442a808ba
pcorrell <pcorrell@vt.edu>
2017-09-05 13:02:22 -0400

    paul-changes: added bptw logo changed charts and updated nav

--- src/Case/assets/js/plugins/charts.js
+++ src/Case/assets/js/plugins/charts.js
@@ -1,44 +1,48 @@
 window.onload = function(){
+  Chart.defaults.global.defaultFontColor = 'black';
 
-Chart.defaults.global.defaultFontColor = 'black';
+  var ctx1 = document.getElementById("pie").getContext('2d');
+  var colors = ["#9b59b6","#f1c40f","#e74c3c","#34495e"];
+  data = [12, 19, 3, 17, 28, 24, 7];
+  var myChart = new Chart(ctx1, {
+    type: 'doughnut',
+    data: {
+      labels: ["Ruby", "Javascript", "Java"],
+      datasets: [{
+        backgroundColor: colors ,
+        data: [47, 32, 21]
+      }]
+    },
+    options: {
+    }
+  });
 
-var ctx1 = document.getElementById("pie").getContext('2d');
-var colors = ["#2ecc71","#3498db","#95a5a6","#9b59b6","#f1c40f","#e74c3c","#34495e"];
-data = [12, 19, 3, 17, 28, 24, 7];
-var myChart = new Chart(ctx1, {
-  type: 'doughnut',
-  data: {
-    labels: ["People who like it", "People who don't like it", "People who don't know what's going on", "People who don't care", "The Uninitiated", "Lord of the Ring fans", "Fell asleep during the movie"],
-    datasets: [{
-      backgroundColor: colors ,
-      data: [12, 19, 3, 17, 28, 24, 7]
-    }]
-
-  }
-  
-
-});
-
-var ctx2 = document.getElementById("bar").getContext('2d');
-var myChart = new Chart(ctx2, {
-  type: 'bar',
-  data: {
-    labels: ["People who like it", "People who don't like it", "People who don't know what's going on", "People who don't care", "The Uninitiated", "Lord of the Ring fans", "Fell asleep during the movie"],
-    datasets: [{
-      backgroundColor: [
-        "#2ecc71",
-        "#3498db",
-        "#95a5a6",
-        "#9b59b6",
-        "#f1c40f",
-        "#e74c3c",
-        "#34495e"
-      ],
-      data: [12, 19, 3, 17, 28, 24, 7]
-    }]
-
-  }
-
-
-});
+  var ctx2 = document.getElementById("bar").getContext('2d');
+  var myChart = new Chart(ctx2, {
+    type: 'horizontalBar',
+    data: {
+      labels: ["Bachelors Degree", "Masters Degree"],
+      datasets: [{
+        backgroundColor: ["#2ecc71", "#3498db"],
+        data: [100, 53]
+      }]
+    },
+    options: {
+      legend: {
+        display: false
+      },
+      scales: {
+          xAxes: [{
+              ticks: {
+                  min: 0,
+                  max: 100
+              }
+          }],
+          scaleLabel: {
+            labelString: "%",
+            display: true
+          }
+      }
+    }
+  });
 };
