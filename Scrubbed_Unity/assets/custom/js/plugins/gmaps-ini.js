b3bcab7a3e34d8836edfade2fb9100e9a51de90d
Andrew Cooper <acooper@consultwithcase.com>
2018-06-06 11:06:40 -0400

    new js files

---
+++ Scrubbed_Unity/assets/custom/js/plugins/gmaps-ini.js
@@ -0,0 +1,53 @@
+// https://developers.google.com/maps/documentation/javascript/examples/
+function initMap() {
+	var customMapType = new google.maps.StyledMapType([
+			{
+				stylers: [
+					{'saturation': -100},
+					{'lightness': 51},
+					{'visibility': 'simplified'}
+				]
+			},
+			{
+				elementType: 'labels',
+				stylers: [{visibility: 'on'}]
+			},
+			{
+				featureType: 'water',
+				stylers: [{color: '#f7f4f4'}]
+			}
+		], {
+			name: 'Case View'
+	});
+
+	var image = new google.maps.MarkerImage(
+		'assets/img/marker.png',
+		new google.maps.Size(48,54),
+		new google.maps.Point(0,0),
+		new google.maps.Point(24,54)
+		);
+
+	var customMapTypeId = 'custom_style';
+
+	var map = new google.maps.Map(document.getElementById('map'), {
+		zoom: 12,
+		scrollwheel: false,
+		center: {lat: 39.108, lng: -77.635},
+		mapTypeControlOptions: {
+			mapTypeIds: [google.maps.MapTypeId.ROADMAP, customMapTypeId]
+		}
+	});
+
+	var infowindow = new google.maps.InfoWindow;
+	infowindow.setContent('<b>Leesburg</b>');
+
+	var marker = new google.maps.Marker({
+		map: map,
+		clickable: false,
+		icon: image,
+		position: {lat: 39.108, lng: -77.635563}
+	});
+
+	map.mapTypes.set(customMapTypeId, customMapType);
+	map.setMapTypeId(customMapTypeId);
+}
