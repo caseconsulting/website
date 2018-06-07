6b2f2917715e374a6c2d4f35001d3d167c57f4ae
Andrew Cooper <acooper@consultwithcase.com>
2018-06-07 15:09:04 -0400

    footer

--- Scrubbed_Unity/assets/custom/js/plugins/gmaps-ini-rtl.js
+++ Scrubbed_Unity/assets/custom/js/plugins/gmaps-ini-rtl.js
@@ -1,53 +1,67 @@
 // https://developers.google.com/maps/documentation/javascript/examples/
 function initMap() {
-	var customMapType = new google.maps.StyledMapType([
-			{
-				stylers: [
-					{'saturation': -100},
-					{'lightness': 51},
-					{'visibility': 'simplified'}
-				]
-			},
-			{
-				elementType: 'labels',
-				stylers: [{visibility: 'on'}]
-			},
-			{
-				featureType: 'water',
-				stylers: [{color: '#bac6cb'}]
-			}
-		], {
-			name: 'Case'
-	});
+  var customMapType = new google.maps.StyledMapType([{
+      stylers: [{
+          'saturation': -100
+        },
+        {
+          'lightness': 51
+        },
+        {
+          'visibility': 'simplified'
+        }
+      ]
+    },
+    {
+      elementType: 'labels',
+      stylers: [{
+        visibility: 'on'
+      }]
+    },
+    {
+      featureType: 'water',
+      stylers: [{
+        color: '#bac6cb'
+      }]
+    }
+  ], {
+    name: 'Case'
+  });
 
-	var image = new google.maps.MarkerImage(
-		'../assets/img/marker.png',
-		new google.maps.Size(48,54),
-		new google.maps.Point(0,0),
-		new google.maps.Point(24,54)
-		);
+  var image = new google.maps.MarkerImage(
+    './assets/img/marker.png',
+    new google.maps.Size(48, 54),
+    new google.maps.Point(0, 0),
+    new google.maps.Point(24, 54)
+  );
 
-	var customMapTypeId = 'custom_style';
+  var customMapTypeId = 'custom_style';
 
-	var map = new google.maps.Map(document.getElementById('map'), {
-		zoom: 12,
-		scrollwheel: false,
-		center: {lat: 40.674, lng: -73.946},  // Reston.
-		mapTypeControlOptions: {
-			mapTypeIds: [google.maps.MapTypeId.ROADMAP, customMapTypeId]
-		}
-	});
+  var map = new google.maps.Map(document.getElementById('map'), {
+    zoom: 12,
+    scrollwheel: false,
+    center: {
+      lat: 40.674,
+      lng: -73.946
+    }, // Reston.
+    mapTypeControlOptions: {
+      mapTypeIds: [google.maps.MapTypeId.ROADMAP, customMapTypeId]
+    }
+  });
 
-	var infowindow = new google.maps.InfoWindow;
-	infowindow.setContent('<b>Reston</b>');
+  var infowindow = new google.maps.InfoWindow;
+  infowindow.setContent('<b>Reston</b>');
 
-	var marker = new google.maps.Marker({
-		map: map,
-		clickable: false,
-		icon: image,
-		position: {lat: 40.674, lng: -73.946} //Reston
-	});
+  var marker = new google.maps.Marker({
+    map: map,
+    clickable: false,
+    icon: image,
+    position: {
+      lat: 40.674,
+      lng: -73.946
+    } //Reston
+  });
 
-	map.mapTypes.set(customMapTypeId, customMapType);
-	map.setMapTypeId(customMapTypeId);
+  map.mapTypes.set(customMapTypeId, customMapType);
+  map.setMapTypeId(customMapTypeId);
 }
